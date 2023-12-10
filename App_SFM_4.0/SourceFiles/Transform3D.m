7dbcd4f959c56183f1cc46d7a914f855f97ad4c5
VAlikV <129046142+VAlikV@users.noreply.github.com>
2023-12-10 20:03:01 +0300

    Add files via upload

---
+++ App_SFM_4.0/SourceFiles/Transform3D.m
@@ -0,0 +1,3 @@
+function [X, Y, Z, B] = Transform3D(lx, ly, lz, nx, ny, nz, center, Bdes) % Преобразование массива вида Node или ROI в трехмерный массив для построения
+[X, Y, Z] = ndgrid(linspace(-lx/2,lx/2,nx)+center(1),linspace(-ly/2,ly/2,ny)+center(2),linspace(-lz/2,lz/2,nz)+center(3));
+B = reshape(Bdes,[nx,ny,nz]);    
---
+++ App_SFM_4.0/SourceFiles/zum.m
@@ -0,0 +1,5 @@
+function s = zum(Arr) % Сумма массива
+s = 0;
+for i=1:length(Arr)
+    s = s + Arr(i);
+end
